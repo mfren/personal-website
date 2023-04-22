9d0a3366c24f46433c75dd50cfba52153f8b6836
Matt French <57752799+MattAdamFrench@users.noreply.github.com>
2023-04-22 15:50:11 +0000

    Restructured Content

--- src/components/Home/Skills/Skills.tsx
+++ src/components/Home/Skills/Skills.tsx
@@ -1,5 +1,95 @@
-import { skills } from "@/content/Home/content"
+"use client";
 
+import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image"
+import { Fragment, useState } from "react"
+import { SKILLS } from "@/content/skills";
+
+function SkillModal(props: { showModal: boolean, setShowModal: Function, skill: string, logo?: string, children?: any }) {
+    return (
+        <Transition appear show={props.showModal} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => props.setShowModal(false)}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                <div className="flex flex-row flex-nowrap justify-start items-center gap-2">
+                                    {props.logo && <Image src={props.logo} alt={props.skill} width={20} className="h-8 w-8" />}
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-lg font-medium leading-6 text-gray-900"
+                                    >
+                                        {props.skill}
+                                    </Dialog.Title>
+                                    <button
+                                        type="button"
+                                        className="ml-auto bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none transition ease-in-out"
+                                        onClick={() => props.setShowModal(false)}
+                                    >
+                                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <hr className="mt-2 mb-2"/>
+                                <div>
+                                    {props.children}
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    )
+}
+
+
+
+
+function SkillBox(props: { skill: string, twColor: string, description: string, logo?: string }) {
+
+    const [showModal, setShowModal] = useState(false)
+
+    return (
+        <>
+            <div onClick={() => setShowModal(true)} className={`flex flex-row gap-2 items-center pt-1 pb-1 pl-1 pr-4 border rounded bg-white border-white drop-shadow-md hover:drop-shadow-xl hover:cursor-pointer`}>
+                {props.logo &&
+                    <Image src={props.logo} alt={props.skill} width={20} className="h-8 w-8 mx-auto" />
+                }
+                <p className="font-light text-center select-none">{props.skill}</p>
+            </div>
+            <SkillModal 
+                showModal={showModal} 
+                setShowModal={setShowModal}
+                skill={props.skill} 
+                logo={props.logo}
+            >
+                <p className="text-gray-500 text-sm">{props.description}</p>
+            </SkillModal>
+        </>
+    )
+}
 
 export function Skills() {
 
@@ -7,32 +97,11 @@ export function Skills() {
         <div className="w-full bg-gray-700">
             <div id="skills" className="container pt-20 pb-20">
                 <h3 className="text-gray-100 font-semibold text-5xl text-center mb-3">Skills</h3>
-                
-                <div className="grid grid-cols-4 gap-4">
-                    <div>
-                        <h4 className="font-semibold text-xl">Languages</h4>
-                        {skills.Languages.map((language, index) => (
-                            <p className="font-light" key={index}>{language}</p>
-                        ))}
-                    </div>
-                    <div>
-                        <h4 className="font-semibold text-xl">Frameworks</h4>
-                        {skills.Frameworks.map((framework, index) => (
-                            <p className="font-light" key={index}>{framework}</p>
-                        ))}
-                    </div>
-                    <div>
-                        <h4 className="font-semibold text-xl">Databases</h4>
-                        {skills.Databases.map((database, index) => (
-                            <p className="font-light" key={index}>{database}</p>
-                        ))}
-                    </div>
-                    <div>
-                        <h4 className="font-semibold text-xl">Tools</h4>
-                        {skills.Tools.map((tool, index) => (
-                            <p className="font-light" key={index}>{tool}</p>
-                        ))}
-                    </div>
+
+                <div className="flex flex-row flex-wrap justify-center gap-4">
+                    {SKILLS.map((skill, index) => (
+                        <SkillBox skill={skill.name} twColor="pink" logo={skill.logo} description={skill.description} key={index} />
+                    ))}
                 </div>
             </div>
         </div>
---
+++ src/content/skills.ts
@@ -0,0 +1,54 @@
+import javascriptLogo from "../../public/svgs/javascript.svg";
+import typescriptLogo from "../../public/svgs/typescript.svg";
+import pythonLogo from "../../public/svgs/python.svg";
+import javaLogo from "../../public/svgs/java.svg";
+import csharpLogo from "../../public/svgs/csharp.svg";
+
+type Skill = {
+    name: string,
+    logo: any,
+    type: SkillType,
+    description: string,
+} 
+
+enum SkillType {
+    Language, Framework, Library, Database, Other
+}
+
+export const JavaScript: Skill = {
+    name: "JavaScript",
+    logo: javascriptLogo,
+    type: SkillType.Language,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem."
+}
+
+export const TypeScript: Skill = {
+    name: "TypeScript",
+    logo: typescriptLogo,
+    type: SkillType.Language,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem."
+}
+
+export const Python: Skill = {
+    name: "Python",
+    logo: pythonLogo,
+    type: SkillType.Language,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem."
+}
+
+export const Java: Skill = {
+    name: "Java",
+    logo: javaLogo,
+    type: SkillType.Language,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem."
+}
+
+export const CSharp: Skill = {
+    name: "C#",
+    logo: csharpLogo,
+    type: SkillType.Language,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem."
+}
+export const SKILLS: Skill[] = [
+    JavaScript, TypeScript, Python, Java, CSharp
+];
